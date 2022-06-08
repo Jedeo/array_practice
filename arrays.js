ad72d1ce8be829cde9bc4900c71ab47398f02e26
Eric Weissman <eric@example.com>
2022-06-08 10:42:13 -0600

    Add array methods

--- arrays.js
+++ arrays.js
@@ -9,3 +9,12 @@ var num = [2, 4, 16, 32];
 //arrays of booleans
 
 var true_or_false = [age = false, car = false, job = true, name = true];
+
+//push will add "spring" at the end of the array
+cities.push("spring");
+
+//will return an a array of 2 elemement of index 1,2
+cities. silice(2,3);
+
+// this will remove the last element from the array
+cities.pop();
