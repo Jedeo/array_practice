d04353b2b565a36514fdfc4ed992bdce791b5744
Eric Weissman <eric@example.com>
2022-06-08 10:32:51 -0600

    Initial commit with arrays declartion

---
+++ arrays.js
@@ -0,0 +1,11 @@
+// variable that stores aarays of strings
+
+var cities = ["Denver", "Fort Collin", "Boulder", "Golden"];
+
+//array of number
+
+var num = [2, 4, 16, 32];
+
+//arrays of booleans
+
+var true_or_false = [age = false, car = false, job = true, name = true];
